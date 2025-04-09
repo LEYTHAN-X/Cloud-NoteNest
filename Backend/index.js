63a4c9efccff0199a3f2c906a3935c630b549c57
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2025-04-09 19:12:26 +0530

    change-3#

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 //Avalible Routes
+app.use(express.json());
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 app.listen(port, () => {
