b03639dc13d8ed4528b19769e5cca54367bece0a
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2025-05-18 12:32:28 +0530

    using locationHook

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,8 @@ import NoteContext from './noteContext';
 import React, { useState } from 'react';
 
 const NoteState = (props) => {
-    const s1 = {
-        name: "Ashish Kumar",
-        class: "5th"
-    }
-    const [state, setstate] = useState(s1);
-    const update=()=>{
-        setTimeout(() => {
-            setstate({
-                name: "Ashish Kumar",
-                class: "6th"
-            })
-        }, 1000);
-    }
-
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
             {props.children}
         </NoteContext.Provider>
     );
