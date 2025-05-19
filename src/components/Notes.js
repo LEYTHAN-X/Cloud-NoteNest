610fcc4c0232732426be089665fe89e523928794
ASHISH KUMAR <152021705+LEYTHAN-X@users.noreply.github.com>
2025-05-19 12:22:16 +0530

    add Note functions

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,17 +1,21 @@
 import React, { useContext } from 'react';
 import noteContext from "../context/notes/noteContext";
 import Noteitem from './Noteitem';
+import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(noteContext);
-    const { notes } = context;
+    const { notes ,addNote} = context;
 
     return (
+        <>
+        <AddNote/>
         <div className="container my-3">
             <h1>Your Notes</h1>
             <div className="row">
                 {notes.map((note) => {
                     return (
+                        
                         <div className="col-md-4 my-2" key={note._id}>
                             <Noteitem note={note} />
                         </div>
@@ -19,6 +23,7 @@ const Notes = () => {
                 })}
             </div>
         </div>
+        </>
     );
 };
 
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const NoteState = (props) => {
     const notesInitial = [{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "6828210qf299f40e085sbdd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -11,7 +11,7 @@ const NoteState = (props) => {
         "date": "2025-05-17T05:39:27.872Z",
         "__v": 0
     },{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "6828df210f299f40e08q5bdd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -19,7 +19,7 @@ const NoteState = (props) => {
         "date": "2025-05-17T05:39:27.872Z",
         "__v": 0
     },{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "682q8210f2s99f40e085bdd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -27,7 +27,7 @@ const NoteState = (props) => {
         "date": "2025-05-17T05:39:27.872Z",
         "__v": 0
     },{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "6828210f299qf4rw0e085bdd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -35,7 +35,7 @@ const NoteState = (props) => {
         "date": "2025-05-17T05:39:27.872Z",
         "__v": 0
     },{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "6q828210f299f40egv085bdd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -43,7 +43,7 @@ const NoteState = (props) => {
         "date": "2025-05-17T05:39:27.872Z",
         "__v": 0
     },{
-        "_id": "6828210f299f40e085bdd7cb",
+        "_id": "6828210fsd299f40e085bdqd7cb",
         "user": "68281f23299f40e085bdd7b7",
         "title": "i am king of this world",
         "description": "on your command king",
@@ -52,8 +52,33 @@ const NoteState = (props) => {
         "__v": 0
     }]
     const [notes, setNotes] = useState(notesInitial);
+
+    // Add a note
+    const addNote=(title,description,tag)=>{
+        console.log("Adding a new note");
+        const note={
+        "_id": "6828210fsd299f40e085bdqd7cb",
+        "user": "68281f23299f40e085bdd7b7",
+        "title": title,
+        "description": description,
+        "tag": tag,
+        "date": "2025-05-17T05:39:27.872Z",
+        "__v": 0
+        };
+        setNotes(notes.concat(note))
+    }
+    
+    // Delete a note
+    const deleteNote=()=>{
+
+    }
+    
+    // Edit a note
+    const editNote=()=>{
+
+    }
     return (
-        <NoteContext.Provider value={{notes,setNotes}}>
+        <NoteContext.Provider value={{notes,addNote,deleteNote,editNote}}>
             {props.children}
         </NoteContext.Provider>
     );
